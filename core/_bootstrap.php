shish/shimmie2

Added $user_config global based on existing config object for storing user-specific settings. Added event to the user page so that extensions can hook into it, providing user-specific setting controls

core/_bootstrap.php
@@ -47,6 +47,11 @@ unset($themelet);
 $page = class_exists("CustomPage") ? new CustomPage() : new Page();
 $_tracer->end();
 
+$_shm_ctx->log_start("Loading user information");
+$user = _get_user();
+$user_config = new DatabaseConfig($database, "user_config","user_id", $user->id);
+$_shm_ctx->log_endok();
+
 // hook up event handlers
 $_tracer->begin("Loading extensions");
 _load_event_listeners();

eltanschauung/Shimmie-Elixir

core/_bootstrap.php
@@ -68,7 +68,13 @@ unset($themelet);
 $page = class_exists("CustomPage") ? new CustomPage() : new Page();
 $_tracer->end();
 
-$_tracer->begin("Loading extensions/event listeners");
+$_tracer->log_start("Loading user information");
+$user = _get_user();
+$user_config = new DatabaseConfig($database, "user_config","user_id", $user->id);
+$_tracer->log_endok();
+
+// hook up event handlers
+$_tracer->begin("Loading event listeners");
 _load_event_listeners();
 $_tracer->end();
 
shish/shimmie2

[tag_edit] separate responsibility for tags, source, owner, and lock

ext/post_lock/main.php
@@ -1,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class LockSetEvent extends Event
+{
+    public Image $image;
+    public bool $locked;
+
+    public function __construct(Image $image, bool $locked)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->locked = $locked;
+    }
+}
+
+class PostLock extends Extension
+{
+    /** @var PostLockTheme */
+    protected Themelet $theme;
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        if (!empty($event->metadata['locked'])) {
+            send_event(new LockSetEvent($event->image, $event->metadata['locked']));
+        }
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($event->image->is_locked() && !$user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            throw new PermissionDenied("Error: This image is locked and cannot be edited.");
+        }
+        if ($user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            $locked = isset($event->params['tag_edit__locked']) && $event->params['tag_edit__locked'] == "on";
+            send_event(new LockSetEvent($event->image, $locked));
+        }
+    }
+
+    public function onLockSet(LockSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            $event->image->set_locked($event->locked);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_lock_editor_html($event->image), 42);
+    }
+}

ext/post_lock/theme.php
@@ -1,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+use function MicroHTML\{INPUT};
+
+class PostLockTheme extends Themelet
+{
+    public function get_lock_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        return SHM_POST_INFO(
+            "Locked",
+            $image->is_locked() ? "Yes (Only admins may edit these details)" : "No",
+            $user->can(Permissions::EDIT_IMAGE_LOCK) ? INPUT(["type" => "checkbox", "name" => "tag_edit__locked", "checked" => $image->is_locked()]) : null
+        );
+    }
+}

ext/post_owner/main.php
@@ -1,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class OwnerSetEvent extends Event
+{
+    public Image $image;
+    public User $owner;
+
+    public function __construct(Image $image, User $owner)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->owner = $owner;
+    }
+}
+
+class PostOwner extends Extension
+{
+    /** @var PostOwnerTheme */
+    protected Themelet $theme;
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        // FIXME: send an event instead of implicit default owner?
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($user->can(Permissions::EDIT_IMAGE_OWNER) && isset($event->params['tag_edit__owner'])) {
+            $owner = User::by_name($event->params['tag_edit__owner']);
+            if ($owner instanceof User) {
+                send_event(new OwnerSetEvent($event->image, $owner));
+            } else {
+                throw new UserNotFound("Error: No user with that name was found.");
+            }
+        }
+    }
+
+    public function onOwnerSet(OwnerSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_OWNER)) {
+            $event->image->set_owner($event->owner);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_owner_editor_html($event->image), 39);
+    }
+}

ext/post_owner/theme.php
@@ -1,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+
+use function MicroHTML\{TR, TH, TD, emptyHTML, rawHTML, joinHTML, DIV, INPUT, A, TEXTAREA};
+
+class PostOwnerTheme extends Themelet
+{
+    public function get_owner_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        $owner = $image->get_owner()->name;
+        $date = rawHTML(autodate($image->posted));
+        $ip = $user->can(Permissions::VIEW_IP) ? rawHTML(" (" . show_ip($image->owner_ip, "Post posted {$image->posted}") . ")") : "";
+        $info = SHM_POST_INFO(
+            "Uploader",
+            emptyHTML(A(["class" => "username", "href" => make_link("user/$owner")], $owner), $ip, ", ", $date),
+            $user->can(Permissions::EDIT_IMAGE_OWNER) ? INPUT(["type" => "text", "name" => "tag_edit__owner", "value" => $owner]) : null
+        );
+        // SHM_POST_INFO returns a TR, let's sneakily append
+        // a TD with the avatar in it
+        $info->appendChild(
+            TD(
+                ["width" => "80px", "rowspan" => "4"],
+                rawHTML($image->get_owner()->get_avatar_html())
+            )
+        );
+        return $info;
+    }
+}

ext/post_source/info.php
@@ -1,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class PostSourcetInfo extends ExtensionInfo
+{
+    public const KEY = "post_source";
+
+    public string $key = self::KEY;
+    public string $name = "Source Editor";
+    public bool $core = true;
+    public string $url = self::SHIMMIE_URL;
+    public array $authors = self::SHISH_AUTHOR;
+    public ExtensionCategory $category = ExtensionCategory::METADATA;
+    public string $description = "Allow images to have sources assigned to them";
+}

ext/post_source/main.php
@@ -1,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class SourceSetEvent extends Event
+{
+    public Image $image;
+    public ?string $source;
+
+    public function __construct(Image $image, string $source = null)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->source = trim($source);
+    }
+}
+
+class PostSource extends Extension
+{
+    /** @var PostSourceTheme */
+    protected Themelet $theme;
+
+    public function onPageRequest(PageRequestEvent $event): void
+    {
+        global $user, $page;
+        if ($event->page_matches("tag_edit/mass_source_set", method: "POST", permission: Permissions::MASS_TAG_EDIT)) {
+            $this->mass_source_edit($event->req_POST('tags'), $event->req_POST('source'));
+            $page->set_mode(PageMode::REDIRECT);
+            $page->set_redirect(search_link());
+        }
+    }
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        if(!empty($event->metadata['source'])) {
+            send_event(new SourceSetEvent($event->image, $event->metadata['source']));
+        }
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($user->can(Permissions::EDIT_IMAGE_SOURCE) && isset($event->params['tag_edit__source'])) {
+            if (isset($event->params['tag_edit__tags']) ? !preg_match('/source[=|:]/', $event->params["tag_edit__tags"]) : true) {
+                send_event(new SourceSetEvent($event->image, $event->params['tag_edit__source']));
+            }
+        }
+    }
+
+    public function onSourceSet(SourceSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_SOURCE)) {
+            $event->image->set_source($event->source);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_source_editor_html($event->image), 41);
+    }
+
+    public function onTagTermCheck(TagTermCheckEvent $event): void
+    {
+        if (preg_match("/^source[=|:](.*)$/i", $event->term)) {
+            $event->metatag = true;
+        }
+    }
+
+    public function onTagTermParse(TagTermParseEvent $event): void
+    {
+        if (preg_match("/^source[=|:](.*)$/i", $event->term, $matches)) {
+            $source = ($matches[1] !== "none" ? $matches[1] : null);
+            send_event(new SourceSetEvent(Image::by_id($event->image_id), $source));
+        }
+    }
+
+    private function mass_source_edit(string $tags, string $source): void
+    {
+        $tags = Tag::explode($tags);
+
+        $last_id = -1;
+        while (true) {
+            // make sure we don't look at the same images twice.
+            // search returns high-ids first, so we want to look
+            // at images with lower IDs than the previous.
+            $search_forward = $tags;
+            if ($last_id >= 0) {
+                $search_forward[] = "id<$last_id";
+            }
+
+            $images = Search::find_images(limit: 100, tags: $search_forward);
+            if (count($images) == 0) {
+                break;
+            }
+
+            foreach ($images as $image) {
+                send_event(new SourceSetEvent($image, $source));
+                $last_id = $image->id;
+            }
+        }
+    }
+}

ext/post_source/theme.php
@@ -1,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+
+use function MicroHTML\{TR, TH, TD, emptyHTML, rawHTML, joinHTML, DIV, INPUT, A, TEXTAREA};
+
+class PostSourceTheme extends Themelet
+{
+    public function mss_html(string $terms): string
+    {
+        $h_terms = html_escape($terms);
+        $html = make_form(make_link("tag_edit/mass_source_set")) . "
+				<input type='hidden' name='tags' value='$h_terms'>
+				<input type='text' name='source' value=''>
+				<input type='submit' value='Set Source For All' onclick='return confirm(\"This will mass-edit all sources on the page.\\nAre you sure you want to do this?\")'>
+			</form>
+		";
+        return $html;
+    }
+
+    public function get_source_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        return SHM_POST_INFO(
+            "Source Link",
+            DIV(
+                ["style" => "overflow: hidden; white-space: nowrap; max-width: 350px; text-overflow: ellipsis;"],
+                $this->format_source($image->get_source())
+            ),
+            $user->can(Permissions::EDIT_IMAGE_SOURCE) ? INPUT(["type" => "text", "name" => "tag_edit__source", "value" => $image->get_source()]) : null,
+            link: Extension::is_enabled(SourceHistoryInfo::KEY) ? make_link("source_history/{$image->id}") : null,
+        );
+    }
+
+    protected function format_source(string $source = null): HTMLElement
+    {
+        if (!empty($source)) {
+            if (!str_contains($source, "://")) {
+                $source = "https://" . $source;
+            }
+            $proto_domain = explode("://", $source);
+            $h_source = $proto_domain[1];
+            if (str_ends_with($h_source, "/")) {
+                $h_source = substr($h_source, 0, -1);
+            }
+            return A(["href" => $source], $h_source);
+        }
+        return rawHTML("Unknown");
+    }
+}

eltanschauung/Shimmie-Elixir

ext/post_lock/main.php
@@ -1,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class LockSetEvent extends Event
+{
+    public Image $image;
+    public bool $locked;
+
+    public function __construct(Image $image, bool $locked)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->locked = $locked;
+    }
+}
+
+class PostLock extends Extension
+{
+    /** @var PostLockTheme */
+    protected Themelet $theme;
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        if (!empty($event->metadata['locked'])) {
+            send_event(new LockSetEvent($event->image, $event->metadata['locked']));
+        }
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($event->image->is_locked() && !$user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            throw new PermissionDenied("Error: This image is locked and cannot be edited.");
+        }
+        if ($user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            $locked = isset($event->params['locked']) && $event->params['locked'] == "on";
+            send_event(new LockSetEvent($event->image, $locked));
+        }
+    }
+
+    public function onLockSet(LockSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_LOCK)) {
+            $event->image->set_locked($event->locked);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_lock_editor_html($event->image), 42);
+    }
+}

ext/post_lock/theme.php
@@ -1,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+
+use function MicroHTML\{INPUT};
+
+class PostLockTheme extends Themelet
+{
+    public function get_lock_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        return SHM_POST_INFO(
+            "Locked",
+            $image->is_locked() ? "Yes (Only admins may edit these details)" : "No",
+            $user->can(Permissions::EDIT_IMAGE_LOCK) ? INPUT(["type" => "checkbox", "name" => "locked", "checked" => $image->is_locked()]) : null
+        );
+    }
+}

ext/post_owner/main.php
@@ -1,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class OwnerSetEvent extends Event
+{
+    public Image $image;
+    public User $owner;
+
+    public function __construct(Image $image, User $owner)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->owner = $owner;
+    }
+}
+
+class PostOwner extends Extension
+{
+    /** @var PostOwnerTheme */
+    protected Themelet $theme;
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        // FIXME: send an event instead of implicit default owner?
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($user->can(Permissions::EDIT_IMAGE_OWNER) && isset($event->params['owner'])) {
+            $owner = User::by_name($event->params['owner']);
+            if ($owner instanceof User) {
+                send_event(new OwnerSetEvent($event->image, $owner));
+            } else {
+                throw new UserNotFound("Error: No user with that name was found.");
+            }
+        }
+    }
+
+    public function onOwnerSet(OwnerSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_OWNER)) {
+            $event->image->set_owner($event->owner);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_owner_editor_html($event->image), 39);
+    }
+}

ext/post_owner/theme.php
@@ -1,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+
+use function MicroHTML\{TR, TH, TD, emptyHTML, rawHTML, joinHTML, DIV, INPUT, A, TEXTAREA};
+
+class PostOwnerTheme extends Themelet
+{
+    public function get_owner_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        $owner = $image->get_owner()->name;
+        $date = rawHTML(autodate($image->posted));
+        $ip = $user->can(Permissions::VIEW_IP) ? rawHTML(" (" . show_ip($image->owner_ip, "Post posted {$image->posted}") . ")") : "";
+        $info = SHM_POST_INFO(
+            "Uploader",
+            emptyHTML(A(["class" => "username", "href" => make_link("user/$owner")], $owner), $ip, ", ", $date),
+            $user->can(Permissions::EDIT_IMAGE_OWNER) ? INPUT(["type" => "text", "name" => "owner", "value" => $owner]) : null
+        );
+        // SHM_POST_INFO returns a TR, let's sneakily append
+        // a TD with the avatar in it
+        $info->appendChild(
+            TD(
+                ["width" => "80px", "rowspan" => "4"],
+                rawHTML($image->get_owner()->get_avatar_html())
+            )
+        );
+        return $info;
+    }
+}

ext/post_source/info.php
@@ -1,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class PostSourceInfo extends ExtensionInfo
+{
+    public const KEY = "post_source";
+
+    public string $key = self::KEY;
+    public string $name = "Source Editor";
+    public bool $core = true;
+    public string $url = self::SHIMMIE_URL;
+    public array $authors = self::SHISH_AUTHOR;
+    public ExtensionCategory $category = ExtensionCategory::METADATA;
+    public string $description = "Allow images to have sources assigned to them";
+}

ext/post_source/main.php
@@ -1,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class SourceSetEvent extends Event
+{
+    public Image $image;
+    public ?string $source;
+
+    public function __construct(Image $image, string $source = null)
+    {
+        parent::__construct();
+        $this->image = $image;
+        $this->source = trim($source);
+    }
+}
+
+class PostSource extends Extension
+{
+    /** @var PostSourceTheme */
+    protected Themelet $theme;
+
+    public function onPageRequest(PageRequestEvent $event): void
+    {
+        global $user, $page;
+        if ($event->page_matches("tag_edit/mass_source_set", method: "POST", permission: Permissions::MASS_TAG_EDIT)) {
+            $this->mass_source_edit($event->req_POST('tags'), $event->req_POST('source'));
+            $page->set_mode(PageMode::REDIRECT);
+            $page->set_redirect(search_link());
+        }
+    }
+
+    public function onImageAddition(ImageAdditionEvent $event): void
+    {
+        if(!empty($event->metadata['source'])) {
+            send_event(new SourceSetEvent($event->image, $event->metadata['source']));
+        }
+    }
+
+    public function onImageInfoSet(ImageInfoSetEvent $event): void
+    {
+        global $page, $user;
+        if ($user->can(Permissions::EDIT_IMAGE_SOURCE) && isset($event->params['source'])) {
+            if (isset($event->params['tags']) ? !preg_match('/source[=|:]/', $event->params["tags"]) : true) {
+                send_event(new SourceSetEvent($event->image, $event->params['source']));
+            }
+        }
+    }
+
+    public function onSourceSet(SourceSetEvent $event): void
+    {
+        global $user;
+        if ($user->can(Permissions::EDIT_IMAGE_SOURCE)) {
+            $event->image->set_source($event->source);
+        }
+    }
+
+    public function onImageInfoBoxBuilding(ImageInfoBoxBuildingEvent $event): void
+    {
+        $event->add_part($this->theme->get_source_editor_html($event->image), 41);
+    }
+
+    public function onTagTermCheck(TagTermCheckEvent $event): void
+    {
+        if (preg_match("/^source[=|:](.*)$/i", $event->term)) {
+            $event->metatag = true;
+        }
+    }
+
+    public function onTagTermParse(TagTermParseEvent $event): void
+    {
+        if (preg_match("/^source[=|:](.*)$/i", $event->term, $matches)) {
+            $source = ($matches[1] !== "none" ? $matches[1] : null);
+            send_event(new SourceSetEvent(Image::by_id($event->image_id), $source));
+        }
+    }
+
+    private function mass_source_edit(string $tags, string $source): void
+    {
+        $tags = Tag::explode($tags);
+
+        $last_id = -1;
+        while (true) {
+            // make sure we don't look at the same images twice.
+            // search returns high-ids first, so we want to look
+            // at images with lower IDs than the previous.
+            $search_forward = $tags;
+            if ($last_id >= 0) {
+                $search_forward[] = "id<$last_id";
+            }
+
+            $images = Search::find_images(limit: 100, tags: $search_forward);
+            if (count($images) == 0) {
+                break;
+            }
+
+            foreach ($images as $image) {
+                send_event(new SourceSetEvent($image, $source));
+                $last_id = $image->id;
+            }
+        }
+    }
+}

ext/post_source/theme.php
@@ -1,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroHTML\HTMLElement;
+
+use function MicroHTML\{TR, TH, TD, emptyHTML, rawHTML, joinHTML, DIV, INPUT, A, TEXTAREA};
+
+class PostSourceTheme extends Themelet
+{
+    public function mss_html(string $terms): string
+    {
+        $h_terms = html_escape($terms);
+        $html = make_form(make_link("tag_edit/mass_source_set")) . "
+				<input type='hidden' name='tags' value='$h_terms'>
+				<input type='text' name='source' value=''>
+				<input type='submit' value='Set Source For All' onclick='return confirm(\"This will mass-edit all sources on the page.\\nAre you sure you want to do this?\")'>
+			</form>
+		";
+        return $html;
+    }
+
+    public function get_source_editor_html(Image $image): HTMLElement
+    {
+        global $user;
+        return SHM_POST_INFO(
+            "Source Link",
+            DIV(
+                ["style" => "overflow: hidden; white-space: nowrap; max-width: 350px; text-overflow: ellipsis;"],
+                $this->format_source($image->get_source())
+            ),
+            $user->can(Permissions::EDIT_IMAGE_SOURCE) ? INPUT(["type" => "text", "name" => "source", "value" => $image->get_source()]) : null,
+            link: Extension::is_enabled(SourceHistoryInfo::KEY) ? make_link("source_history/{$image->id}") : null,
+        );
+    }
+
+    protected function format_source(string $source = null): HTMLElement
+    {
+        if (!empty($source)) {
+            if (!str_contains($source, "://")) {
+                $source = "https://" . $source;
+            }
+            $proto_domain = explode("://", $source);
+            $h_source = $proto_domain[1];
+            if (str_ends_with($h_source, "/")) {
+                $h_source = substr($h_source, 0, -1);
+            }
+            return A(["href" => $source], $h_source);
+        }
+        return rawHTML("Unknown");
+    }
+}